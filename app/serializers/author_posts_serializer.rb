9093ebdabfb1fa93b9c2bbaf57d8ce36db3fd029
Robert Sainton <robertsainton@outlook.com>
2023-02-10 01:04:14 -0500

    assignment completed

--- app/serializers/author_posts_serializer.rb
+++ app/serializers/author_posts_serializer.rb
@@ -3,7 +3,7 @@ class AuthorPostsSerializer < ActiveModel::Serializer
   has_one :profile
   has_many :posts
   has_many :post_tags
-  has_many :tags, through: :post_tags
+  has_many :tags
   def short_content
     "#{self.object.content[0..39]}..."
   end
